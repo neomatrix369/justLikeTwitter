fb993f8eb7845d56ff06212d78d660435c166f23
neomatrix369 <sadhak001@gmail.com>
2015-08-19 00:53:48 +0100

    Refactored ConsoleUI to simplify the implementation, make it more human readable

--- src/main/java/interfaces/ConsoleUI.java
+++ src/main/java/interfaces/ConsoleUI.java
@@ -8,9 +8,23 @@ public class ConsoleUI {
     private static final String COMMAND_PROMPT_INDICATOR = "> ";
 
     public String showPrompt() throws IOException {
-        System.out.print(COMMAND_PROMPT_INDICATOR);
+        printPromptOnScreen();
 
-        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferRead = gatherWhatUserTypesAtThePrompt();
+
+        return returnWhatUserHasTyped(bufferRead);
+    }
+
+    private void printPromptOnScreen() {
+        System.out.print(COMMAND_PROMPT_INDICATOR);
+    }
+
+    private BufferedReader gatherWhatUserTypesAtThePrompt() {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        return new BufferedReader(inputStreamReader);
+    }
+
+    private String returnWhatUserHasTyped(BufferedReader bufferRead) throws IOException {
         return bufferRead.readLine();
     }
 }
